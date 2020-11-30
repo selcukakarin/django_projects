03ad88ddf83c632b7009494c6e53711495916d47
selcukakarin <selcukakarin@novumare.com.tr>
2020-11-30 15:07:32 +0300

    requirements are added.

--- jquery_date_picker/jqueryDatePicker/urls.py
+++ jquery_date_picker/jqueryDatePicker/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('article/',include("article.urls"))
+    path('',include("article.urls"))
 ]
